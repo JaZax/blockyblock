d0ae2be45c22fe1f5a8593d2fe7a23db6fb1c551
JaZax <iza.janeczekk@gmail.com>
2020-11-09 11:27:36 +0100

    added colors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { gsap } from 'gsap'
 
 import './style.scss'
 import Block from './components/block.js'
 
 class App extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.appRef = React.createRef()
+    }
+
+    componentDidMount() {
+        document.body.addEventListener('click', (e)=>{
+            //this.blockRef.current.style.border = 'solid green'
+            console.log(e.target)
+            //console.log(this.appRef.current)
+        })
+    }
+    
+    componentWillUnmount() {
+        document.body.removeEventListener("click");
+    }
 
     render(){
         const elements = new Array(40);
@@ -13,13 +29,27 @@ class App extends React.Component {
         const items = []
 
         for (const [index] of elements.entries()) {
-            items.push(<Block id={index}></Block>)
+            let classString = ''
+
+            if(index < 8){
+                classString = 'block row1'
+            }else if(index < 16 && index >= 8){
+                classString = 'block row2'
+            }else if(index < 24 && index >= 16){
+                classString = 'block row3'
+            }else if(index < 32 && index >= 24){
+                classString = 'block row4'
+            }else if(index < 40 && index >= 32){
+                classString = 'block row5'
+            }
+            
+            items.push(<Block class={classString} id={index}></Block>)
         }
 
         return(
-            <>
+            <div id="wrap" ref={this.appRef}>
                 {items}
-            </>
+            </div>
         )
     }
 }
--- src/components/block.js
+++ src/components/block.js
@@ -6,24 +6,28 @@ class Block extends React.Component {
         super(props)
 
         this.blockRef = React.createRef();
+        this.idString = ''
+
     }
 
+    /*
     componentDidMount() {
         this.blockRef.current.addEventListener('click', ()=>{
+            //this.blockRef.current.style.border = 'solid green'
             console.log(this.blockRef.current.id)
         })
     }
     
     componentWillUnmount() {
-        document.body.removeEventListener("click");
+        this.blockRef.current.removeEventListener("click");
     }
+    */
 
     render(){
         return(
-            <div ref={this.blockRef} className="block" id={this.props.id}></div>
+            <div ref={this.blockRef} className={this.props.class} id={this.props.id}></div>
         )
     }
-
 }
 
 export default Block
